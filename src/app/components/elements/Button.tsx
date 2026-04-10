f18caf141e2f3edd1fa70725399206cb17816d50
makoto-shimura <shimura.mak@mandt-sys.com>
2026-04-10 10:38:56 +0900

    ログイン画面修正

--- src/app/components/elements/Button.tsx
+++ src/app/components/elements/Button.tsx
@@ -5,7 +5,24 @@ type ButtonProps = React.ComponentProps<typeof MuiButton>
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
-    <MuiButton variant="contained" {...props}>
+    <MuiButton
+      variant="contained"
+      sx={{
+        background: "#86171F",
+        color: "white",
+        padding: "12px 32px",
+        borderRadius: "8px",
+        fontWeight: "bold",
+        transition: "0.2s",
+        boxShadow: "none",
+        '&:hover': {
+          background: "#6e1218",
+          transform: "translateY(-2px)",
+          boxShadow: "none",
+        },
+      }}
+      {...props}
+    >
       {props.children}
     </MuiButton>
   )
